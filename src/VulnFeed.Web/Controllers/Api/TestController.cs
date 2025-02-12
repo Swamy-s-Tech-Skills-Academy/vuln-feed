657c2f204089bcfca75f4cb8281012444c47c9c6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-02-12 23:27:10 +0530

    Added Sample controllers for Api, and Web

---
+++ src/VulnFeed.Web/Controllers/Api/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VulnFeed.Web.Controllers.Api;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { Message = "Hello from VulnFeed.Web API!" });
+    }
+}
