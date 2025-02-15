c152b74bf0fcda0b9c2beeea894c74fe427e9fb0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-02-15 17:13:40 +0530

    Changes (#6)

---
+++ src/VulnFeed.Common/Http/HttpLink.cs
@@ -0,0 +1,18 @@
+﻿using VulnFeed.Common.Validation;
+
+namespace VulnFeed.Common.Http;
+
+// record without positional parameters
+public sealed record HttpLink
+{
+    public string Rel { get; init; }
+
+    public string Location { get; init; }
+
+    public HttpLink(string rel, string location)
+    {
+        Rel = rel.RequireNonEmpty(nameof(rel));
+
+        Location = location.RequireNonEmpty(nameof(location));
+    }
+}
